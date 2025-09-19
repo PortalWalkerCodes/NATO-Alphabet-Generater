073aa981ebf5900eda33e6bda3ed5695f94de9f7
Atticus <atticus.beard@icloud.com>
2025-09-19 17:24:40 -0600

    Finished it!

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
+from art import logo
+import os
+import time
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+program_is_running = True
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+while program_is_running:
+    nato_words = {row.letter:row.code for (index, row) in df.iterrows()}
+    print(logo)
+    input_word = input("Enter a word or type exit to exit: ").upper()
+
+    nato_word = [nato_words[letter] for letter in input_word]
+    print(nato_word)
+    input("Press Enter to continue...\n")
+
+    if input_word == "EXIT":
+        program_is_running = False
+    else:
+        clear_screen()
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
